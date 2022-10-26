d63b19c2c671534634283d3ffbca02f75a1086d4
Colin Marshall <colin.marshall94@gmail.com>
2022-10-26 15:27:01 -0700

    added dynamically updating timeblocks using Moment.  Will use this to compare against current time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,40 @@
-// GIVEN I am using a daily planner to create a schedule
-
-// WHEN I open the planner
-// THEN the current day is displayed at the top of the calendar
-// DONE
 var today = moment();
+var todayString = "Current unix timestamp: ";
+console.log(todayString + today.format('X'));
+
 var hour;
 //sets first hour of day in a variable to increment
-var startHour = 9;
+var timeBlockHour = 9;
 var $hour = $(".hour");
+var $today =$("#currentDay")
 
 //sets current day and time in header
-$("#currentDay").text(today.format("MMM Do, YYYY, hh:mm:ss"));
+$today.text(today.format("MMM Do, YYYY, hh:mmA"));
+// $today.text(today.format("X"));
 
+//sets the time of each timeblock dynamically so it can be compared to unix timestamp
+for (i=0; i<$hour.length; i++) {
+  hour = today.set('hour', timeBlockHour);
+  $hour.eq(i).text(hour.format("hA"));
+  console.log(hour.format('X'));
+  console.log(parseInt(hour.format('X')));
+  timeBlockHour++;
+}
 
-hour = today.set('hour', startHour);
-$hour.text(hour.format("hA"));
-
-$hour.each(function(index) {
-    console.log(startHour)
-    console.log(index + ": " + $(this).text() );
-    startHour++;
-    console.log(startHour)
-  });
-
-
-//dynamically update following .hour tags incrementing++ by array class
-
-//moment() go get the current day dynamically
-
-// WHEN I scroll down
-// THEN I am presented with timeblocks for standard business hours
-// DONE?  Does this need to be created using jquery instead of writing HTML?
+// This changed 9am to unix timestamp on the actual web app...wtf?
+// console.log($hour.eq().text(hour.format("X")));
 
 // WHEN I view the timeblocks for that day
 // THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+// PSEUDO
+// Compare to unix timestamp
+// if (today > hour) {
+//   apply class .past
+// } else if ( today = hour) {
+//   apply class .present
+// } else {
+//   apply class .future
+// }
 
 // WHEN I click into a timeblock
 // THEN I can enter an event
