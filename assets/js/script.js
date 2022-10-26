870469aee9cb0e91277bdf6645b5e927ce8f6b42
Colin Marshall <colin.marshall94@gmail.com>
2022-10-26 16:54:51 -0700

    dynamically generated unix timestamps for each timeblock now stored in nineToFive[]

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,11 @@
 var today = moment();
-var todayString = "Current unix timestamp: ";
-console.log(todayString + today.format('X'));
+var text = "Current unix timestamp: ";
+var todayNumber = parseInt(today.format('X'));
+console.log(today.format('X'));
+console.log(todayNumber);
 
+var nineToFive = [];
+var hourUnixTimestamp
 var hour;
 //sets first hour of day in a variable to increment
 var timeBlockHour = 9;
@@ -16,17 +20,19 @@ $today.text(today.format("MMM Do, YYYY, hh:mmA"));
 for (i=0; i<$hour.length; i++) {
   hour = today.set('hour', timeBlockHour);
   $hour.eq(i).text(hour.format("hA"));
-  console.log(hour.format('X'));
-  console.log(parseInt(hour.format('X')));
+  nineToFive.push(parseInt(hour.format('X')));
+  
+  // console.log(parseInt(hour.format('X')));
   timeBlockHour++;
 }
 
-// This changed 9am to unix timestamp on the actual web app...wtf?
-// console.log($hour.eq(0).text(hour.format("X")));
+console.log (nineToFive);
+//nineToFive is now an array storing each timeblock as its index (i.e. nineToFive[0] = 9am), just need to hook it up to each element and can then compare them to current time to apply CSS class
 
 // WHEN I view the timeblocks for that day
 // THEN each timeblock is color coded to indicate whether it is in the past, present, or future
 // PSEUDO
+
 // Compare to unix timestamp
 // if (today > hour) {
 //   apply class .past
